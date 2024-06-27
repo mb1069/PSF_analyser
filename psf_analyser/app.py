0f343add0d2cac4a75ed401fcb85d833366f3dc8
Miguel Boland <migueldboland@gmail.com>
2024-06-27 11:49:24 +0100

    Reverted app.py

--- psf_analyser/app.py
+++ psf_analyser/app.py
@@ -1,57 +1,31 @@
 import dash
-from dash import Dash, html
+from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
-from flask import Flask
-import threading
-from werkzeug.serving import make_server
-import time
+
 DEV_MODE = True
 
-keepPlot=True
-def stop_execution():
-    global keepPlot
-    #stream.stop_stream()
-    keepPlot=False
-    # stop the Flask server
-    server.shutdown()
-    server_thread.join()
-    print("Dash app stopped gracefully.")
-    
+def main():
+    dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
+    app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.DARKLY, dbc_css])
 
-dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
-# app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.DARKLY, dbc_css])
-server = Flask(__name__)
-app = Dash(__name__, server=server, use_pages=True, external_stylesheets=[dbc.themes.DARKLY, dbc_css])
-
-
-# Home page layout
-app.layout = dbc.Container([
-    html.H1('PSF analyser'),
-    html.A(children=html.P('To home page'), href='/'),
-    dash.page_container
-])
-
-args = {
-    # 'suppress_callback_exceptions': not DEV_MODE,
-    'debug': DEV_MODE,
-    'dev_tools_ui': DEV_MODE,
-    'dev_tools_props_check': DEV_MODE
-}
-    
+    app.layout = dbc.Container([
+        html.H1('PSF analyser'),
+        html.A(children=html.P('To home page'), href='/'),
+        # html.Div([
+        #     html.Div(
+        #         dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
+        #     ) for page in dash.page_registry.values()
+        # ]),
+        dash.page_container
+    ])
+    args = {
+        # 'suppress_callback_exceptions': not DEV_MODE,
+        'debug': DEV_MODE,
+        'dev_tools_ui': DEV_MODE,
+        'dev_tools_props_check': DEV_MODE
+    }
+    app.run(host='0.0.0.0', **args)
+    print('Running!')
 
 if __name__ == '__main__':
-    # create a server instance
-    server = make_server("localhost", 8050, server)
-    # start the server in a separate thread
-    server_thread = threading.Thread(target=server.serve_forever)
-    server_thread.start()
-
-    # start the Dash app in a separate thread
-    def start_dash_app():
-        app.run_server(debug=True, use_reloader=False)
-
-    dash_thread = threading.Thread(target=start_dash_app)
-    dash_thread.start()
-
-    while keepPlot:
-        time.sleep(1)  # keep the main thread alive while the other t
+    main()
